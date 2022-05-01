6e61710d90a9fc0ee55084cd5460579e6ec919b9
Tobias Boege <tobs@taboege.de>
2022-05-01 13:28:43 +0200

    Fix Relation usage bug and improve memory usage
    The memory usage is reduced for the typical applications of semimatroid
    or orientability testing, which cache parts of their setup depending on
    the cube they operate on. Many cubes lead to high memory usage, when
    though most of the cubes in selfadhesivity testing are isomorphic.
    This commits permutes the the relation instead so that we have only
    one new cube per cardinality in the selfadhesivity testing.
    
    There was one severe bug in the algorithm when it accessed the internals
    of the CInet::Relation $S -- in the meantime the implementation details
    changed.

--- lib/CInet/Adhesive.pm
+++ lib/CInet/Adhesive.pm
@@ -10,19 +10,36 @@ use Array::Set qw(set_diff);
 use List::Util qw(all);
 use Algorithm::Combinatorics qw(subsets);
 
-sub is_selfadhesive_at {
+sub is_selfadhesive_at :Export(:DEFAULT) {
     my ($x, $realiz, $I) = @_;
     my $Ncube = Cube($x);
     my $N = $Ncube->set;
     my $K = set_diff($N, $I);
+
+    # $realiz algorithms tend to cache parts of their setup based on the
+    # ->cube of their argument. By permuting $I to the initial segment of
+    # $N avoids the creation of lots of isomorphic cubes and the caches
+    # associated to them. This assumes that $realiz is invariant under
+    # isomorphy!
+    my $p = do {
+        my $p;
+        my @elts = @$N; # copy of @$N in its natural order
+        $p->[$_] = shift(@elts)
+            for $Ncube->_index(@$I, @$K); # (@$I, @$K) in that order!
+        $p
+    };
+    $I = [ $N->@[0 .. $I->$#*] ];
+    $K = set_diff($N, $I);
+    $x = $x->permute($p);
+
     # Make a copy of N but prefix all axes not in I with a string to make
     # them different from their N preimages. Then join the two cubes.
-    my $prefix = join '', sort @$I;
+    my $prefix = 0+ $I->@*;
     my %NtoM = (
-        map({ $_ =>      $_      } @$I),
+        map({ $_ =>     "$_"     } @$I),
         map({ $_ => "$prefix^$_" } @$K),
     );
-    my $M = map $NtoM{$_}, @$N;
+    my $M = [ map $NtoM{$_}, @$N ];
     my $L = set_diff($M, $I);
 
     my $NMcube = Cube [@$K, @$L, @$I];
@@ -34,8 +51,7 @@ sub is_selfadhesive_at {
     for my $ijK ($Ncube->squares) {
         # Translate N-symbol to M-symbol
         my $MijK = [ map { [ map $NtoM{$_}, @$_ ] } @$ijK ];
-        $S->[$NMcube->pack($ijK)] = $S->[$NMcube->pack($MijK)] =
-            $x->[$Ncube->pack($ijK)];
+        $S->cival($ijK) = $S->cival($MijK) = $x->cival($ijK);
     }
     # 3. The independence (K,L|I) must hold. To encode the global CI
     # statement as a conjunction of local ones, we assume only the
@@ -52,9 +68,9 @@ sub is_selfadhesive_at {
     $realiz->($S)
 }
 
-sub is_selfadhesive {
+sub is_selfadhesive :Export(:DEFAULT) {
     my ($x, $realiz) = @_;
-    return all { selfadhesive_at $x, $realiz, $_ }
+    return all { is_selfadhesive_at $x, $realiz, $_ }
         map { $_->[1] } Cube($x)->vertices
 }
 
